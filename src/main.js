b5515ace0a3a470025687e0bb55f728e1a2a897c
MichaelHebert <evogashira@gmail.com>
2016-06-02 17:01:05 -0400

    reactin' bad

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-let greeting = "hi";
-console.log(greeting);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+let greetingElement = <div>
+<h1 id="special-greeting" className="header-greeting">
+  Sup Polka Posies
+</h1>
+</div>
+
+ReactDOM.render(
+  greetingElement,
+  document.getElementById('greeting')
+)
